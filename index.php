739c655bd1b79f080528fedcfd081daaa1ab1b5d
Klvst3r <klvst3r@gmail.com>
2017-02-01 18:52:29 -0600

    Se corrige Index

--- index.php
+++ index.php
@@ -6,69 +6,91 @@
 		<link rel="stylesheet" href="bootstrap/css/bootstrap.css">
 		<link rel="stylesheet" href="bootstrap/css/bootstrap-responsive.css">
 		<link rel="stylesheet" type="text/css" href="css/style.css">
-	<title>Login-Registro</title>
+	<title>Proyecto academias</title>
 </head>
 <body>
-	<center><div class="tit"><h2>Inicio de sesión</h2>
-		<center><div class="Ingreso">
 
-	<table border="0" align="center" valign="middle">
-		<tr>
-		<td rowspan=2>
-		<form action="validate.php" method="post">
+<center><div class="tit"><h2 style="color: #0000FF; ">Inicio de sesión</h2>
+    <center><div class="Ingreso">
 
-		<table border="0">
+<form action="validar.php" method="post">
+<center>
+	<table border="0" width="350">
 
-		<tr><td><label><b>Email: </b></label></td>
+		<tr><td><label style="font-size: 14pt"><b>Correo: </b></label></td>
 			<td width=80> <input class="form-group has-success" style="border-radius:15px;" type="text" name="mail"></td></tr>
-		<tr><td><label><b>Password: </b></label></td>
+		<tr><td><label style="font-size: 14pt"><b>Contraseña: </b></label></td>
 			<td witdh=80><input style="border-radius:15px;" type="password" name="pass"></td></tr>
-		<tr><td></td>
-			<td width=80 align=center><input class="btn btn-primary" type="submit" value="Aceptar"></td>
-			</tr></tr></table>
-		</form>
-<br>
-<!-- Register Form -->
+      <tr>
+          <td colspan="2"> &nbsp;</td>
+          
+        </tr>
 
+		<tr><td colspan="3">
+        <center>
+        <input class="btn btn-primary" type="submit" value="Aceptar">
+        </center>
+        </td>
+        
+	  </tr></table>
+</center>
+
+</form>
+        
+<br/><br/>    
+
+<!-- formulario registro -->       
 <form method="post" action="" >
-  <fieldset>
+<fieldset>
     <legend  style="font-size: 18pt"><b>Registro</b></legend>
+
+<table height="150" width="350" border="0" align="center" valign="middle">
+		<tr>
+		<td>
+
     <div class="form-group">
-      <label><b>Ingresa tu nombre</b></label>
+      <label style="font-size: 14pt"><b>Ingresa tu nombre</b></label>
       <input type="text" name="realname" class="form-control" placeholder="Ingresa tu nombre" />
     </div>
+    
     <div class="form-group">
-      <label><b>Ingresa tu email</b></label>
+      <label style="font-size: 14pt"><b>Ingresa tu email</b></label>
       <input type="text" name="nick" class="form-control"  required placeholder="Ingresa mail"/>
     </div>
+
     <div class="form-group">
-      <label><b>Ingresa tu Password</b></label>
+      <label style="font-size: 14pt"><b>Ingresa tu Password</b></label>
       <input type="password" name="pass" class="form-control"  placeholder="Ingresa contraseña" />
     </div>
+
     <div class="form-group">
-      <label><b>Repite tu contraseña</b></label>
+      <label style="font-size: 14pt"><b>Repite tu contraseña</b></label>
       <input type="password" name="rpass" class="form-control" required placeholder="repite contraseña" />
     </div>
-      
+
+    <div>
+   <center>
+    <input  class="btn btn-danger" type="submit" name="submit" value="Registrarse"/>
+    </center>
     </div>
-   
-    <input  class="btn btn-danger" type="submit" name="submit" value="Registrar"/>
 
-  </fieldset>
-</form>
-</div>
 <?php
-		if(isset($_POST['submit'])){
-			require("register.php");
-		}
-	?>
-<!--Fin formulario registro -->
 
-		</td>
+if(isset($_POST['submit'])){
+      require("registro.php");
+    }
+
+ ?>
+
+
+    </td>
+
 		</tr>
 		</table>
-		</div></center></div></center>
+      
+<!--Fin formulario registro -->
+ </fieldset>
+ </form>
 
-	
 </body>
 </html>
